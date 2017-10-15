80aaef9b0ec9c10639a8dca871a91a2b87ec1347
Willian Z <Willian@Willian-Zhang.com>
2017-10-14 20:46:14 -0400

    dl num

--- scripts/dl.sh
+++ scripts/dl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-head -n 1 data/wet.paths | awk '{print "https://commoncrawl.s3.amazonaws.com/"$1}' | xargs -n1 wget -P data/wet
+head -n $1 data/wet.paths | awk '{print "https://commoncrawl.s3.amazonaws.com/"$1}' | xargs -n1 wget -P data/wet
