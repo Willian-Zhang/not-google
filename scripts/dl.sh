752745c37d7f128f83f71de27658ab1dece69a77
Willian Z <Willian@Willian-Zhang.com>
2017-10-17 07:55:13 -0400

    Refine dl

--- scripts/dl.sh
+++ scripts/dl.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-head -n $1 data/wet.paths | awk '{print "https://commoncrawl.s3.amazonaws.com/"$1}' | xargs -n1 wget -P data/wet
+mkdir -p "data/wet"
+head -n $1 "data/wet.paths" | awk '{print "https://commoncrawl.s3.amazonaws.com/"$1}' | xargs -n1 wget -P "data/wet"
