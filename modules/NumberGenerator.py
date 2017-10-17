de963cf1c51a1d24b033b94a5791aea1c018d5cf
Willian Z <Willian@Willian-Zhang.com>
2017-10-17 03:20:27 -0400

    Fix docID

--- modules/NumberGenerator.py
+++ modules/NumberGenerator.py
@@ -3,12 +3,16 @@
 
 tbyte = int.from_bytes(b'\t','little',signed=True)
 nbyte = int.from_bytes(b'\n','little',signed=True)
+sbyte = int.from_bytes(b' ','little',signed=True)
 
 class Number:
-    def __init__(self, digits = 4, start = 0):
+    """
+    Get Numbers avoiding '\t' '\n' and <space>
+    """
+    def __init__(self, digits = 4, after = 0):
         self.digits = digits
         self.byte_orders = range(0, 8*digits, 8)
-        self.i = start
+        self.i = after
     
     def __iter__(self):
         return self
@@ -16,11 +20,19 @@ class Number:
     def __next__(self):
         return self.next()
 
-    def next(self):
-        # TODO: better implrementation, support space
-        move = [1 if x^tbyte == 0 or x^nbyte == 0 else 0 
-                 for x in self.i.to_bytes(self.digits,'little', signed=True)]
-        move = sum([should_move << move_power for (should_move, move_power) in zip(move, self.byte_orders)])
-        self.i += move + 1
-        return self.i - 1
+    def _get_move(self, number):
+        move = [1 if b^tbyte == 0 or b^nbyte == 0 or b^sbyte == 0 else 0
+                for b in number.to_bytes(self.digits, 'little', signed=True)]
+        return sum([should_move << move_power
+                    for (should_move, move_power) in zip(move, self.byte_orders)])
 
+    def next(self):
+        """
+        Get next Number avoiding '\t' '\n' and <space>
+        """
+        move = 1
+        while move > 0:
+            self.i += move
+            move = self._get_move(self.i)
+
+        return self.i
